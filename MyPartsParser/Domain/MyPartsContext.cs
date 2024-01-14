06d0b185b5470a570488c2798559bdc2af8f44ec
Levan Shanava <shanavalevani@gmail.com>
2024-01-14 17:09:04 +0400

    change dbcontext

--- MyPartsParser/Domain/MyPartsContext.cs
+++ MyPartsParser/Domain/MyPartsContext.cs
@@ -10,7 +10,7 @@ namespace MyPartsParser.Domain
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=MyAuto; Trusted_Connection=True;Connection Timeout=600");
-                //optionsBuilder.UseSqlServer("Data Source=10.64.4.21; Initial Catalog=Bureau; user id=pabuser;password=u6JVj6lAU9nLJh1FaDpr");
+               // optionsBuilder.UseSqlServer("Data Source=10.64.4.21; Initial Catalog=MyParts; user id=pabuser;password=u6JVj6lAU9nLJh1FaDpr");
             }
         }
     }
